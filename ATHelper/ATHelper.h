24501d2eafc662cabb03fcdc541ad2a11dcb1d4b
Allen Huang <allen.w.huang@gmail.com>
2016-05-01 23:33:11 +0800

    UPDATE: Add init files

---
+++ ATHelper/ATHelper.h
@@ -0,0 +1,19 @@
+//
+//  ATHelper.h
+//  ATHelper
+//
+//  Created by allen huang on 2016/5/1.
+//  Copyright © 2016年 AppTrek Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATHelper.
+FOUNDATION_EXPORT double ATHelperVersionNumber;
+
+//! Project version string for ATHelper.
+FOUNDATION_EXPORT const unsigned char ATHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATHelper/PublicHeader.h>
+
+
